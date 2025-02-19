25efaffe67d56c85f39d5f01b1988f961b512f1e
Borel KOUMO <borel.koumo_ext@michelin.com>
2025-02-19 18:12:03 +0100

    initial-commit

---
+++ mongoose-vs-native-driver/mongoose-server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+app.use(express.json())
+
+// Connexion à MongoDB avec gestion des erreurs
+mongoose
+    .connect('mongodb://localhost:27020/mongoose-vs-native-driver-mongoose')
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => {
+        console.error(' MongoDB connection error:', err)
+        process.exit(1) 
+    });
+
+const db = mongoose.connection
+
+// Définition du schéma et du constructeur
+const PersonSchema = new mongoose.Schema({
+    civility: String,
+    name: String,
+    age: Number,
+})
+const Person = mongoose.model('Person', PersonSchema)
+
+// Route pour ajouter une personne
+app.post('/persons', async (req, res) => {
+    try {
+        // Création et sauvegarde d'une nouvelle personne
+        const { civility, name, age } = req.body
+        const person = new Person({ civility, name, age })
+        const savedPerson = await person.save()
+
+        // Retourner l'objet inséré
+        return res.status(200).json({
+            message: 'Person created successfully',
+            data: { _id: savedPerson._id }, 
+        })
+    } catch (error) {
+        return res.status(500).json({ message: `Error saving person ${error.message}`})
+    }
+})
+
+const PORT = process.argv[2].split('=')[1]
+app.listen(PORT, () => {
+    console.log('Listening on port ' + PORT)
+})
